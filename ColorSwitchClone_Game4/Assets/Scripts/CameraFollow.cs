b76549ce7e212b9194ae2f989544f58e073c1e41
Oğuzhan Töre ÖZYÜREK <37810336+otorezyurek@users.noreply.github.com>
2020-06-28 02:49:51 +0300

    First Commit
    Uploading the Color Switch(ish) project to github for the first time.

---
+++ ColorSwitchClone_Game4/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;    // oyuncunun Transform bileşeni
+    public float lerpValue;    // yumuşak geçiş için lerp değeri
+
+    void LateUpdate()
+    {
+        if (player.position.y > transform.position.y)
+        {
+            Vector3 newPosition = new Vector3(transform.position.x, player.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPosition, lerpValue);
+        }
+    }
+}
---
+++ ColorSwitchClone_Game4/Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    public string color;
+
+    public Color purple;
+    public Color blue;
+    public Color red;
+    public Color yellow;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        setColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+            //rb.velocity = Vector2.zero;
+            //rb.AddForce(Vector2.up * jumpForce);
+
+            rb.velocity = Vector2.up * jumpForce;
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.CompareTag("switcher"))
+        {
+            setColor();
+            //collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+            return;
+        }
+
+        if (!collision.CompareTag(color))
+		{
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}        
+	}
+
+    public void setColor()
+	{
+        int i = Random.Range(0, 4);
+
+		switch (i)
+		{
+            case 0:
+                color = "purple";
+                sr.color = purple;
+                break;
+            
+            case 1:
+                color = "blue";
+                sr.color = blue;
+                break;
+
+            case 2:
+                color = "red";
+                sr.color = red;
+                break;
+
+            case 3:
+                color = "yellow";
+                sr.color = yellow;
+                break;
+        }
+	}
+}
